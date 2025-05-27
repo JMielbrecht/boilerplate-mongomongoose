803d5d88a35a635b61cca9fb25d2580247ed354f
JMielbrecht <jordan.mielbrecht@gmail.com>
2025-05-26 17:55:24 -0700

    create and save a record
    https://www.freecodecamp.org/learn/back-end-development-and-apis/mongodb-and-mongoose/create-and-save-a-record-of-a-model

--- myApp.js
+++ myApp.js
@@ -1,10 +1,27 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const person = new Person({
+    name: "Jimmy", 
+    age: 26,
+    favoriteFoods: ['pizza', 'beer']
+  })
+  person.save().then(data => {
+    data.save()
+    done(null, data);
+  });
+  // done(null, person);
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
